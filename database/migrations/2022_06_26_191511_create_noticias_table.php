b9cede63cd774f7002f8cc3443d965e223f9226a
elizeohamu <elizeohamu@gmail.com>
2022-06-26 16:49:25 -0300

    criacao de database

---
+++ database/migrations/2022_06_26_191511_create_noticias_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNoticiasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('noticias', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('titulo');
+            $table->string('texto');
+            $table->datetime('data');
+
+            $table->integer('categoria_id')->unsigned();
+
+            $table->foreign('categoria_id')->references('id')->on('categorias')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('noticias');
+    }
+}
